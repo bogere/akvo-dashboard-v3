529d8fd654e1f3c7b8cf6b4e885b8260a1909ac6
bogere goldsoft <goldsoft25@gmail.com>
2018-05-02 15:10:02 +0300

    Finished debugging the translation file for English locale

--- app/components/footer-part.js
+++ app/components/footer-part.js
@@ -9,9 +9,9 @@ export default Component.extend({
        { label: "Group 2", value: "group2" }
      ])
      this.set('languageContent', [
-        { label: "EnglishT", value: "English", group: 'group1' },
-        { label: "SpanishT", value: "Spanish", group:'group2' },
-        {label: 'japanishT', value: 'Japanese', group: 'group1'}
+        { label: "EnglishT", value: "English" },
+        { label: "SpanishT", value: "Spanish"},
+        {label: 'japanishT', value: 'Japanese'}
      ])
   },
   actions:{
