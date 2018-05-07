5300c9b9944af20cecdc5216452f33d89dd260fc
bogere goldsoft <goldsoft25@gmail.com>
2018-05-07 14:38:31 +0300

    Dynamically change the layers for the map

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -6,5 +6,20 @@ export default Controller.extend({
   // regular properties to the component, bound to the controller. 
   lat:0.3262600,
   lng:32.6149900, //Hive Collab Kampala
-  zoom: 10
+  zoom: 10,
+  defaultLayer: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+  tileLayers:[
+    {label: 'openstreet', value: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'},
+    {label: 'stamen terrain', value: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png'},
+    {label: 'night',value:'https://cartocdn_{s}.global.ssl.fastly.net/base-midnight/{z}/{x}/{y}.png'},
+    {label: 'Eris', value: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'} 
+  ],
+  actions:{
+    //changing the tile layers dynamically.
+    changeLayer(){
+     var selectedLayer = this.get('selectedOption')
+     console.log(selectedLayer)
+      this.set('defaultLayer', selectedLayer)  
+    }
+  }
 });
