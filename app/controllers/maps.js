709b70acb1873766f4f545ecb3ad5924402eefd1
bogere goldsoft <goldsoft25@gmail.com>
2018-05-07 13:10:03 +0300

    Adding the tile layers to the map

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -4,7 +4,7 @@ export default Controller.extend({
   //for the demo map..
   //Notice that we specified the center of the map and its zoom level passing
   // regular properties to the component, bound to the controller. 
-  lat:45.519743,
-  lng:-122.680522,
+  lat:0.3262600,
+  lng:32.6149900, //Hive Collab Kampala
   zoom: 10
 });
