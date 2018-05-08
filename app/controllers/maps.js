e0182729826def646ddf9bd03bdff422719322aa
bogere goldsoft <goldsoft25@gmail.com>
2018-05-08 13:11:03 +0300

    Adding the markers and popups on the map

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -9,11 +9,15 @@ export default Controller.extend({
   zoom: 10,
   defaultLayer: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
   tileLayers:[
-    {label: 'openstreet', value: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'},
     {label: 'stamen terrain', value: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png'},
+    {label: 'openstreet', value: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'},
     {label: 'night',value:'https://cartocdn_{s}.global.ssl.fastly.net/base-midnight/{z}/{x}/{y}.png'},
     {label: 'Eris', value: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'} 
   ],
+  //catering for the markers on the map..
+  //play around with other geocordinates.
+   peaceLocation: [0.3372743,32.5921307],
+   britishLocation:[0.334949619708498,32.58311441970849],
   actions:{
     //changing the tile layers dynamically.
     changeLayer(){
