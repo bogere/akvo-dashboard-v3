426e27dd4c83706d88a2cb0d7a6c8abfa0cdcda5
bogere goldsoft <goldsoft25@gmail.com>
2018-05-24 07:16:06 +0300

    Finally managed to load JSON data from mirage to the data models

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -2,9 +2,9 @@ import { JSONAPISerializer } from 'ember-cli-mirage';
 import {camelize} from '@ember/string';
 export default JSONAPISerializer.extend({
    //to stop Ember-data from customising my attribute keys..  instead of surveyId, it wants survey-id
-    keyForAttribute(key){
-      //return key;
+    /*keyForAttribute(key){
+      return key;
       //return Ember.String.camelize(key)
-      return camelize(key)
-   }
+      //return camelize(key)
+   }*/
 });
