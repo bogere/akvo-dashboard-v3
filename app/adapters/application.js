3428cb0a2df60cea4d2f404e57fab4f3ef1bd0ff
bogere goldsoft <goldsoft25@gmail.com>
2018-05-24 17:42:01 +0300

    Still figring out  about RESTSerializer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,11 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPIAdapter.extend({
+/*export default DS.JSONAPIAdapter.extend({
   namespace: 'api'
-});
+});*/ //the above is DS.JSONAPIAdapter for the JSON API specs..
+
+export default DS.RESTAdapter.extend({ //REST Adapter
+   namespace: 'api'
+})
+
+
--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend({
+export default DS.RESTSerializer.extend({
     keyForAttribute(key){
       return key;
     }
