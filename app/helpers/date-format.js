527a8b780466963fd1293bd30f065e30e99a582a
bogere goldsoft <goldsoft25@gmail.com>
2018-06-19 14:06:05 +0300

    Finally i can see content passed to the placemarkdetail component

---
+++ app/helpers/date-format.js
@@ -0,0 +1,22 @@
+import { helper } from '@ember/component/helper';
+
+export function dateFormat([dateValue]/*, hash*/) {
+  //return params;
+  //i need to format my date format..
+  console.log('time passed',dateValue)
+  if (dateValue) {
+     var d = new Date(dateValue)
+     var date = d.getDate(),
+         yr = d.getFullYear(),
+         t = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric',hour12:true}); //AM or PM
+    //getting the specific month.
+      var months = ["Jan", "Feb", "Mar", "Apr", "May", "June", 
+                    "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+          m = months[d.getMonth()];
+          
+        return date + ' ' + m + ' ' + yr + ' at ' + t;      
+  }
+   return null
+}
+
+export default helper(dateFormat);
